ff156cfd9cbe750e3adf440789671f6b33498fb6
ttaver <taungon@brandeis.edu>
2018-04-24 18:56:16 -0400

    Add files via upload

---
+++ finalProject.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class finalProject {
+    public static void main(String args[]) {
+
+        //Creating the Frame
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 900);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
+
+        // Panels
+
+        JPanel search = new JPanel(); // the panel is not visible in output
+        JPanel display = new JPanel(new GridLayout(0,1));
+
+
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        mb.add(m1);
+        JMenuItem m11 = new JMenuItem("Open");
+        m1.add(m11);
+        frame.add(mb);
+
+        // Table w scroll pane
+
+        JTable table = new JTable(50, 2);
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        scrollPane.setMinimumSize(new Dimension(480,300));
+        display.add(scrollPane);
+        frame.add(display);
+
+        frame.pack();
+        frame.setVisible(true);
+
+
+
+        // search bar (add event listener)
+        JButton label = new JButton("Search");
+        JTextField tf = new JTextField(10);
+
+        // JButton reset = new JButton("Reset");
+        search.add(label);
+        search.add(tf);
+        frame.add(search);
+        // panel.add(send);
+        // panel.add(reset);
+
+        // JPanel
+
+
+        //JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        // frame.getContentPane().add(BorderLayout.SOUTH, search);
+      //  frame.getContentPane().add(BorderLayout.[]k[]], mb);
+        //frame.getContentPane().add(BorderLayout.CENTER, ta);
+        // frame.setVisible(true);
+    }
+}
