05627e0517db406952bb11903df97114811a035a
Sagar Punjabi <Sagar.punjabi@yahoo.com>
2018-04-24 21:45:33 -0400

    functionality for add button

--- JavaApplication8/src/finalProject.java
+++ JavaApplication8/src/finalProject.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class finalProject {
     File records;
@@ -12,6 +15,18 @@ public class finalProject {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(800, 600));
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
+        
+        /**
+         * Creating Map objects
+         * phoneBook key = name val = number
+         * rphoneBook key = number val = name
+         */
+        
+        Map<String,String>phoneBook;
+        phoneBook = new HashMap<>();
+        
+        Map<String,String>rphoneBook;
+        rphoneBook = new HashMap<>();
 
         /**
          *  Panel
@@ -65,6 +80,29 @@ public class finalProject {
         add.add(phoneTF);
         add.add(addB);
         
+        //function to add button
+        addB.addActionListener((ActionEvent e) -> 
+        {
+            if(nameTF.getText().isEmpty() || phoneTF.getText().isEmpty())
+            {
+                System.out.println("text is not empty");
+                JOptionPane.showMessageDialog(null,"Please Enter A Name And PhoneNumber");
+            }
+            else
+            {
+                System.out.println("info saved");
+                phoneBook.put(nameTF.getText(), phoneTF.getText());
+                rphoneBook.put(phoneTF.getText(), nameTF.getText()); 
+                
+                writeMapToFile(phoneBook,"output.txt");
+
+                nameTF.setText("");
+                phoneTF.setText("");
+                System.out.println(phoneBook.keySet());
+            }
+            
+        });
+        
 
         /** 
          *  SearchBar
@@ -88,6 +126,65 @@ public class finalProject {
         frame.add(search);  // Search bar
         frame.setVisible(true);      
     }
+    
+    /**
+     * This method will write to a file
+     * @param d
+     * Takes in HashMap it will write to the file from
+     * @param filename 
+     * name of the file it will store information too
+     */
+     public static void writeMapToFile(Map<String,String>d,String filename)
+     {
+        System.out.println("method called");
+        System.out.println(filename);
+        try 
+        {
+            System.out.println("we in dis");
+            PrintWriter writer = new PrintWriter(filename, "UTF-8");
+            Set<String> keys = d.keySet();
+            for(String name: keys)
+            {
+                writer.println(name+"|"+d.get(name));
+            }
+            writer.close();
+        } 
+        catch (Exception e)
+        {
+            System.out.println("Problem writing to file: "+e);
+        }
+    }
+     
+    /**
+     * This method will return a Map that can read from a file
+     * @param filename
+     * @return 
+     */
+    public static Map<String,String> readFile( String filename )
+    {
+        Map <String,String> x = new HashMap<String,String>();
+        try
+        {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()){
+          String line = scanner.nextLine();
+          int delimiter = line.indexOf("|");
+          String key = line.substring(0,delimiter);
+          String value = line.substring(delimiter+1);
+          x.put(key,value);
+        }
+        scanner.close();
+        } 
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Problem reading map from file "+e);
+        }
+        return x;
+    } 
+    
+    
+    
 
     public static void main(String args[]) {
         SwingUtilities.invokeLater(() -> {
