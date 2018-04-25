16ea803b7e93aae6f84ad7339481f964f1cf268c
Julian Ho <julianho@Julians-MacBook-Pro.local>
2018-04-24 21:21:21 -0400

    Added filechooser and fixed gui

--- JavaApplication8/src/finalProject.java
+++ JavaApplication8/src/finalProject.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.io.File;
+
 public class finalProject {
+    File records;
     public finalProject() {
         // Creating the Frame
         JFrame frame = new JFrame("Final Project");
@@ -10,58 +13,80 @@ public class finalProject {
         frame.setMinimumSize(new Dimension(800, 600));
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
 
-        // Panels
-
-        JPanel search = new JPanel(); // the panel is not visible in output
+        /**
+         *  Panel
+         */
+        JPanel search = new JPanel(new FlowLayout());
         JPanel display = new JPanel(new GridLayout(0,1));
+        JPanel add = new JPanel(new FlowLayout());
 
 
-
-        // Creating the MenuBar and adding components
+        /**
+         *  Menu Bar
+         */
         JMenuBar mb = new JMenuBar();
         JMenu m1 = new JMenu("FILE");
-        mb.add(m1);
         JMenuItem m11 = new JMenuItem("Open");
+        mb.add(m1);
+        mb.add(Box.createHorizontalGlue());
         m1.add(m11);
-        frame.add(mb);
 
-        // Table w scroll pane
+        /**
+         *  File Chooser
+         */
+        JFileChooser fc = new JFileChooser();
+        m11.addActionListener(e -> {
+            int returnValue = fc.showOpenDialog(null);
 
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                records = fc.getSelectedFile();
+            }
+        });
+
+        /**
+         *  Table
+         */
         JTable table = new JTable(50, 2);
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setMaximumSize(new Dimension(800,300));
         display.add(scrollPane);
-        frame.add(display);
-
-        frame.pack();
-        frame.setVisible(true);
-
-
+        
+        /**
+         *  AddBar
+         *  
+         *  This add bar add a record to the hashmap
+         */
+        Button addB = new Button("Add");
+        TextField nameTF = new TextField(20);
+        TextField phoneTF = new TextField(20);
+        add.add(new Label("Name"));
+        add.add(nameTF);
+        add.add(new Label("Phone number"));
+        add.add(phoneTF);
+        add.add(addB);
+        
 
         /** 
          *  SearchBar
-         *  <p>
+         *  
          *  This search bar searches records against the hashmap
          *  for names and phone numbers
          */
+        Button searchB = new Button("Search");
+        TextField searchTF = new TextField(60);
+        search.add(searchTF);
+        search.add(searchB);
+
+
+
         
-        
-
-         
-        frame.add(search);
-        // panel.add(send);
-        // panel.add(reset);
-
-        // JPanel
 
 
-        //JTextArea ta = new JTextArea();
-
-        //Adding Components to the frame.
-        // frame.getContentPane().add(BorderLayout.SOUTH, search);
-      //  frame.getContentPane().add(BorderLayout.[]k[]], mb);
-        //frame.getContentPane().add(BorderLayout.CENTER, ta);
-        // frame.setVisible(true);
+        frame.add(mb);  // Menu bar
+        frame.add(display);   // Table
+        frame.add(add);     // Add bar
+        frame.add(search);  // Search bar
+        frame.setVisible(true);      
     }
 
     public static void main(String args[]) {
